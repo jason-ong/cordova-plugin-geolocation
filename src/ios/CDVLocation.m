382f9526e91a8b97f8a12d7477c656f5269dc0e8
jason-ong <jasonong91@gmail.com>
2015-09-07 12:19:12 +0800

    Keep updating IOS geolocation
    * dont filter the distance to keep updating ios geolocation

--- src/ios/CDVLocation.m
+++ src/ios/CDVLocation.m
@@ -143,14 +143,16 @@
         __highAccuracyEnabled = YES;
         // Set distance filter to 5 for a high accuracy. Setting it to "kCLDistanceFilterNone" could provide a
         // higher accuracy, but it's also just spamming the callback with useless reports which drain the battery.
-        self.locationManager.distanceFilter = 5;
+        // self.locationManager.distanceFilter = 5;
+        self.locationManager.distanceFilter = kCLDistanceFilterNone;
         // Set desired accuracy to Best.
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     } else {
         __highAccuracyEnabled = NO;
         // TODO: Set distance filter to 10 meters? and desired accuracy to nearest ten meters? arbitrary.
-        self.locationManager.distanceFilter = 10;
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        // self.locationManager.distanceFilter = 10;
+        self.locationManager.distanceFilter = kCLDistanceFilterNone;
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
     }
 }
 
